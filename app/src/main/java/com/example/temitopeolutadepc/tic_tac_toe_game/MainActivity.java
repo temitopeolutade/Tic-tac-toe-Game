903427e49b7900d2bae555d79092342db7a59545
Temitope Olutade <topeolutade@ymail.com>
2018-10-07 08:19:08 +0100

    Created and implemented array for winning positions

--- app/src/main/java/com/example/temitopeolutadepc/tic_tac_toe_game/MainActivity.java
+++ app/src/main/java/com/example/temitopeolutadepc/tic_tac_toe_game/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //0 represents yellow while 1 represents red and 2 means empty
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     int[] gameState={2,2,2,2,2,2,2,2,2};
 
+    int [][] winningPositions ={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+
 
     public void putin(View view){
 
@@ -22,19 +25,21 @@ public class MainActivity extends AppCompatActivity {
         Log.i(" numner",counter.getTag().toString());
 
         int tappedCounter=Integer.parseInt(counter.getTag().toString());
-        gameState[tappedCounter]=activeball;
+
+        gameState[tappedCounter]=activeball;//identify what color is in each box
 
         counter.setTranslationY(-1500);//takes the image above the screen
 
         if (activeball==0) {
 
             counter.setImageResource(R.drawable.yellow);
-           activeball=1;
+          activeball=1;
         }
         else{
             counter.setImageResource(R.drawable.red);
             activeball=0;
         }
+
 
       /** if (activeball){
            counter.setImageResource(R.drawable.yellow);
@@ -46,6 +51,22 @@ public class MainActivity extends AppCompatActivity {
        }**/
 
         counter.animate().translationYBy(1500).setDuration(300);
+
+        String winner =" ";
+
+        for(int[] winningPosition :winningPositions ){
+            if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&  gameState[winningPosition[1]]== gameState[winningPosition[2]] && gameState[winningPosition[0]] !=2);
+
+                //This line compares the gamestate with the winningPosition to see if they all have the same colour of ball
+            if (activeball==1){
+                winner ="Yellow";
+            }
+            else {
+                winner ="Red";
+            }
+
+            Toast.makeText(this,winner + "Has won " , Toast.LENGTH_SHORT).show();
+        }
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
